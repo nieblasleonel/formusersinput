440eea5f5c9a7ab6b6b9a565e10737599e03e05a
Nieblas <nieblasleonel@gmail.com>
2024-02-21 11:09:40 -0600

    using formData

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
+
 export default function Login() {
+
+const [enteredValues, setEnteredValues] = useState({
+  email: '',
+  password: ''
+});
+
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log(enteredValues);
+    setEnteredValues({
+      email: '',
+      password: ''
+    })   
+  }
+
+  const handleInputChange = (identifier,event)=>{
+    setEnteredValues(prevValues=>({
+      ...prevValues,
+      [identifier]: event.target.value
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input 
+               id="email" 
+               type="email" 
+               name="email" 
+               onChange={(event)=>handleInputChange('email',event)}
+               value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input 
+              id="password" 
+              type="password" 
+              name="password" 
+              onChange={(event)=>handleInputChange('password',event)}
+              value={enteredValues.password}
+              />
         </div>
       </div>
 
